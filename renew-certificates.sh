c3a11d1c45474fed2be8bd2744f03d9a840e24d1
Yair <yairopro@gmail.com>
2022-03-29 16:08:52 +0300

    minor

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -10,32 +10,32 @@ gsutil -m rsync -r "${LETSENCRYPT_BUCKET}" /etc/letsencrypt
 # echo "rm -rf /etc/letsencrypt/*"
 # rm -rf /etc/letsencrypt/*
 
-echo "Renewing certificate..."
+# echo "Renewing certificate..."
 
-dns_provider_options="--dns-${DNS_PROVIDER}"
-if [ "${DNS_PROVIDER}" != "route53" ] && [ "${DNS_PROVIDER}" != "google" ]; then
-  echo -e "${DNS_PROVIDER_CREDENTIALS}" > /dns_api_key.ini
-  dns_provider_options="${dns_provider_options} --dns-${DNS_PROVIDER}-credentials /dns_api_key.ini"
-fi
+# dns_provider_options="--dns-${DNS_PROVIDER}"
+# if [ "${DNS_PROVIDER}" != "route53" ] && [ "${DNS_PROVIDER}" != "google" ]; then
+#   echo -e "${DNS_PROVIDER_CREDENTIALS}" > /dns_api_key.ini
+#   dns_provider_options="${dns_provider_options} --dns-${DNS_PROVIDER}-credentials /dns_api_key.ini"
+# fi
 
-echo certbot command: certbot certonly -v -n \
-  -m "${LETSENCRYPT_CONTACT_EMAIL}" --agree-tos \
-  --preferred-challenges dns ${dns_provider_options} \
-  -d "*.${CUSTOM_DOMAIN}"
+# echo certbot command: certbot certonly -v -n \
+#   -m "${LETSENCRYPT_CONTACT_EMAIL}" --agree-tos \
+#   --preferred-challenges dns ${dns_provider_options} \
+#   -d "*.${CUSTOM_DOMAIN}"
 
-certbot certonly -v -n \
-  -m "${LETSENCRYPT_CONTACT_EMAIL}" --agree-tos \
-  --preferred-challenges dns ${dns_provider_options} \
-  -d "*.${CUSTOM_DOMAIN}"
+# certbot certonly -v -n \
+#   -m "${LETSENCRYPT_CONTACT_EMAIL}" --agree-tos \
+#   --preferred-challenges dns ${dns_provider_options} \
+#   -d "*.${CUSTOM_DOMAIN}"
 
-echo "Convert private key into RSA format"
-openssl rsa \
-  -in "/etc/letsencrypt/live/${CUSTOM_DOMAIN}/privkey.pem" \
-  -out "/etc/letsencrypt/live/${CUSTOM_DOMAIN}/privkey-rsa.pem" \
+# echo "Convert private key into RSA format"
+# openssl rsa \
+#   -in "/etc/letsencrypt/live/${CUSTOM_DOMAIN}/privkey.pem" \
+#   -out "/etc/letsencrypt/live/${CUSTOM_DOMAIN}/privkey-rsa.pem" \
 
 
-echo "Backup of letsencrypt context"
-gsutil -m rsync -r /etc/letsencrypt "${LETSENCRYPT_BUCKET}"
+# echo "Backup of letsencrypt context"
+# gsutil -m rsync -r /etc/letsencrypt "${LETSENCRYPT_BUCKET}"
 
 
 echo ""
@@ -53,21 +53,12 @@ cat /etc/letsencrypt/live/${CUSTOM_DOMAIN}/privkey-rsa.pem
 echo ""
 
 echo "Install certificate on App Engine"
-certificate_id=24364635
+certificate_id=25661238
 
-if [ "${certificate_id}" = "" ]; then
-  echo "Creating new certificate"
-  certificate_id=$(gcloud app ssl-certificates create \
-    --display-name "${CERTIFICATE_NAME}" \
-    --certificate "/etc/letsencrypt/live/${CUSTOM_DOMAIN}/fullchain.pem" \
-    --private-key "/etc/letsencrypt/live/${CUSTOM_DOMAIN}/privkey-rsa.pem" \
-    --format "get(id)")
-else
-  echo "Updating existing certificate"
-  gcloud app ssl-certificates update "${certificate_id}" \
-    --certificate "/etc/letsencrypt/live/${CUSTOM_DOMAIN}/fullchain.pem" \
-    --private-key "/etc/letsencrypt/live/${CUSTOM_DOMAIN}/privkey-rsa.pem"
-fi
+echo "Updating existing certificate"
+gcloud app ssl-certificates update "${certificate_id}" \
+	--certificate "/etc/letsencrypt/live/${CUSTOM_DOMAIN}/fullchain.pem" \
+	--private-key "/etc/letsencrypt/live/${CUSTOM_DOMAIN}/privkey-rsa.pem"
 
 echo "Enable certificate on *.${CUSTOM_DOMAIN} domain mapping"
 gcloud app domain-mappings update "*.${CUSTOM_DOMAIN}" --certificate-management manual --certificate-id "${certificate_id}"
